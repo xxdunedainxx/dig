a20115ffef8c9e45b0e20e44e109f6f7d4c61473
Zach McFadden <zach.mcfadden@workday.com>
2021-12-20 11:33:10 -0800

    bump version + update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 mkdir -p logs
 mkdir -p db
-docker-compose down || echo "Docker compose not running, no need to tear down"
+docker-compose down --remove-orphans || echo "Docker compose not running, no need to tear down"
 
 docker-compose up -d
